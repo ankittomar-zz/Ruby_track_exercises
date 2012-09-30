562d6fdf6ae8ba7af83dbb20ead50bd9ec1cade2
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-09-30 19:57:19 +0530

    modified power_array to return modified array

--- Power_Array.rb
+++ Power_Array.rb
@@ -3,16 +3,17 @@ def power_arr
   power_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   print("Enter exponent value: ")
   x = gets().to_i
-  expo_fun(power_array,x)
-end
-
-def expo_fun(power_array,x)
   puts("====== Array Before Modification ======")
   print(power_array.collect{|a| a})
   puts
+  power_array = expo_fun(power_array,x)
   puts("========= Array After Modification ========")
-  print(power_array.collect{|a| a**x})
+  print(power_array.collect{|a| a})
   puts
+end
+
+def expo_fun(power_array,x)
+  power_array.collect{|a| a**x}
 end
 
 power_arr
