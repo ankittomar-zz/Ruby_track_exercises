8de4644f74f8302584e23bc4a1ef0f4e44da4295
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-10-27 16:06:57 +0530

    added highlight exercise

--- highlight_search_result.rb
+++ highlight_search_result.rb
@@ -1,9 +1,8 @@
 def highlight_search
   str = "Can you can a can as a canner can can a can?"
   count =0
-  str.gsub!(/can/i){|c|
-   count+=1 
-   '(can)' }
+  str.gsub!(/can/i){|c| count+=1
+  '(can)' }
   puts str  
 end
 
