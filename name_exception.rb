da6db248c267cb3cf33c09979f525a8252199c5d
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-11-08 22:28:41 +0530

    modifications

--- name_exception.rb
+++ name_exception.rb
@@ -5,7 +5,7 @@ class Name
     puts("Enter Last Name")
     last_name = gets.rstrip
     begin
-      ((first_name[0] =~/^[A-Z]/) && first_name.length > 0 && last_name.length > 0) ? 
+      ((first_name=~/^[A-Z]/) && first_name.length > 0 && last_name.length > 0) ? 
       puts("Name entered is #{first_name} #{last_name}")  : raise(ArgumentError) 
     rescue ArgumentError
       puts("lower case or nil exception raised")
