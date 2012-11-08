73afa349a38a2cf049f15cf7b55303add8f29e11
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-11-08 17:18:56 +0530

    added factorial_exception

---
+++ factorial_exception.rb
@@ -0,0 +1,26 @@
+def factorial
+  puts("enter the number whose factorial is to be calculated: ")
+  number = gets().to_i
+  begin
+    if number<0
+      raise ArgumentError
+    else
+      factorial = generate_factorial(number)
+      puts ("Factorial of #{number} is = #{factorial}") 
+    end
+  rescue ArgumentError
+   puts "Exception occured please try again"
+  end
+end
+
+def generate_factorial(number)
+  return 1 if number < 2  
+  rang = 2..number
+  facto = 1 
+  rang.each {|digit| facto = facto*digit}
+  facto  
+end
+
+factorial
+
+
