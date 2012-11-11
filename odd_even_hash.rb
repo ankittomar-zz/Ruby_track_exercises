a45b9ff5499488575f28e2fe29582ca1d02c757d
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-11-11 11:54:13 +0530

    modify

--- odd_even_hash.rb
+++ odd_even_hash.rb
@@ -1,26 +1,26 @@
 def array_hash
   items = Array.new
   items = ['array','qwerty','qwer',123456,12345,'qwert',1234,123,'qwe',12,'qw',1234567,'qwertyu']
-  arr_hash=arr_to_hash(items)
+  element_count_hash=arr_to_hash(items)
   puts("===================================character count hash=================================")
-  puts arr_hash
+  puts element_count_hash
   puts("==============================Partitioon hash===========================================")
-  puts odd_even(arr_hash) 
+  puts odd_even(element_count_hash) 
 end
 
 def arr_to_hash(items)
-  len=items.length-1
-  arr_hash = Hash.new
-  for i in 0..len
-    elem_len = items[i].to_s.length
-    (arr_hash[elem_len] ||= []) << (items[i])
+  array_length=items.length-1
+  element_count_hash = Hash.new
+  for i in 0..array_length
+    element_length = items[i].to_s.length
+    (element_count_hash[element_length] ||= []) << (items[i])
   end
-  arr_hash
+  element_count_hash
 end
 
-def odd_even(arr_hash)
+def odd_even(element_count_hash)
   part = Hash.new  
-  arr_hash.inject(0) { |hash,(k,v)| if k.odd? then (part["odd"] ||= []) << v  else  (part["even"] ||= []) << v end } 
+  element_count_hash.inject(0) { |hash,(k,v)| if k.odd? then (part["odd"] ||= []) << v  else  (part["even"] ||= []) << v end } 
   part
 end
 
--- ruby_IO/input_output.rb
+++ ruby_IO/input_output.rb
@@ -1,14 +1,14 @@
 def read_file
   filename = 'read.csv'
-  dev_hash = Hash.new
+  develope_hash = Hash.new
   File.open(filename).read.split("\n").each do |line|
     name,emp_id,designation = line.split(',')
     element = (name << " " << "(" << "EmpId: #{emp_id}" << ")")
-    (dev_hash[designation] ||= []) << element
+    (develope_hash[designation] ||= []) << element
   end
   
   File.open('new.txt','w') { |f|
-    dev_hash.each {|key , value|
+    develope_hash.each {|key , value|
       f << "#{key}" << "\n " 
       value.each{|item|
       f << "\t"<< "#{item}"  << "\t" << "\n" }
