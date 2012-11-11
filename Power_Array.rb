a62a57f43949f0349fc578cf02f4a22bf43273be
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-11-11 10:30:15 +0530

    modified

--- Power_Array.rb
+++ Power_Array.rb
@@ -1,17 +1,16 @@
 def power_arr
-  power_array=Array.new
-  power_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  initial_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   print("Enter exponent value: ")
   x = gets().to_i
   puts("====== Array Before Modification ======")
-  p power_array
-  power_array = expo_fun(power_array,x)
+  p initial_array
+  power_array = power(initial_array,x)
   puts("========= Array After Modification ========")
   p power_array
 end
 
-def expo_fun(power_array,x)
-  power_array.collect{|a| a**x}
+def power(initial_array,x)
+  initial_array.collect{|a| a**x}
 end
 
 power_arr
