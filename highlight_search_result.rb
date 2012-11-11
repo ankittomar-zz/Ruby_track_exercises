c43457f896511fd506741e0c0762495ceda30cd6
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-11-11 10:19:32 +0530

    modify

--- highlight_search_result.rb
+++ highlight_search_result.rb
@@ -2,9 +2,9 @@ def highlight_search
   str = "Can you can a can as a canner can can a can?"
   count =0
   str.gsub!(/can/i){|c| count+=1
-  '(can)' }
+  "(#{c})" }
   puts str  
-  puts "count"
+  puts "Number of occurence = #{count}"
 end
 
 highlight_search
