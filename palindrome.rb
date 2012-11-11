6f74c905f489923b067bf15e6f7f1cf713f23a56
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-11-11 12:00:42 +0530

    modify

--- palindrome.rb
+++ palindrome.rb
@@ -2,8 +2,8 @@ def palindrome
   continue = 'y'
   until continue == 'q' || continue == 'Q' do
     print ("enter the string to be checked : ")
-    sen = gets().rstrip.downcase
-    puts check_palindrome(sen) ? "String entered is Palindrome" : "String entered is not Palindrome" 
+    statement = gets().rstrip.downcase
+    puts check_palindrome(statement) ? "String entered is Palindrome" : "String entered is not Palindrome" 
     print("To continue press enter and to exit press 'q' or 'Q' : ")
     continue = gets().rstrip
   end
