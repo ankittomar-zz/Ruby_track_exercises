5bd6c4c5875e8e00fe158ebf2da9ba5e62f09c7f
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-11-30 01:13:26 +0530

    modified interesr_diff.rb

--- interest_difference.rb
+++ interest_difference.rb
@@ -3,23 +3,23 @@ class InterestDifference
   def simple_intrest
      @principal+(@principal*@rate*@time)/100
   end
+  
   def compound_interest
      @principal * ((1+ (@rate/100.0))** @time)
   end
+  
   def difference
     puts ("Difference = #{compound_interest - simple_intrest}")
   end
 end
 
 print("Enter principal :  ")
-principal = gets.to_i
+principal = gets.chomp.to_i
 print("\nenter time : ")
-time = gets.to_i
-obj = InterestDifference.new.tap do |item|
-  item.rate = 10
-  item.time = time
-  item.principal = principal
+time = gets.chomp.to_i
+InterestDifference.new.tap do |item|
+  item.rate,item.time,item.principal = 10,time,principal
+  puts ("Amount with compound_interestpound Intrest = #{item.compound_interest} \n")
+  puts ("Amount with Simple Intrest = #{item.simple_intrest} \n")
+  puts item.difference
 end
-puts ("Amount with Compound Intrest = #{obj.compound_interest} \n")
-puts ("Amount with Simple Intrest = #{obj.simple_intrest} \n")
-puts obj.difference
