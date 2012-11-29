a66c92c180258968232bc38c63c52baf6e9ef125
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-11-30 00:48:54 +0530

    modified hash.rb

--- hash.rb
+++ hash.rb
@@ -1,24 +1,16 @@
-
-def get_string
-  print ("Enter the string to be tested :  ")
-  statement = gets.chomp
-  p(count_character(statement))
-end  	
-
 def count_character(statement)
   count_hash = Hash.new()
   statement.each_char do |alphabet|
-    if alphabet!= "\n" && alphabet != " "
-      if count_hash.assoc(alphabet) == nil 
-        count_hash[alphabet] = 1
-      else 
-        count_hash[alphabet] += 1
-      end
+    if alphabet != " "
+      count_hash.assoc(alphabet).nil? ? count_hash[alphabet] = 1 : count_hash[alphabet] += 1
     end
   end
   count_hash
 end
 
-get_string
+print ("Enter the string to be tested :  ")
+statement = gets.chomp
+p(count_character(statement))
+
 
 
