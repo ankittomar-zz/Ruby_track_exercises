3b517f585803da7018291a95244dca17d82139a3
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-11-29 19:08:11 +0530

    fixed regex

--- regularex.rb
+++ regularex.rb
@@ -2,12 +2,11 @@ def get_text()
   print("enter the text : ")
   statement = gets.chomp
   puts ("String before substitution : #{statement}")
-  replace_vowel(statement)
-  puts ("string after substitution : #{statement}")
+  puts ("string after substitution : #{replace_vowel(statement)}")
 end
 
 def replace_vowel(statement)
-  statement.gsub!(/[aeiou]/i, '*')
+  statement.gsub(/[aeiou]/i, '*')
 end
 get_text
 
