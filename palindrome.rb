37abec891d54566985ed96bb54741387dbd81e3a
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-11-30 02:16:36 +0530

    modifications

--- palindrome.rb
+++ palindrome.rb
@@ -1,16 +1,13 @@
-def palindrome
-  continue = 'y'
-  until continue == 'q' || continue == 'Q' do
-    print ("enter the string to be checked : ")
-    statement = gets.chomp.rstrip.downcase
-    puts check_palindrome(statement) ? "String entered is Palindrome" : "String entered is not Palindrome" 
-    print("To continue press enter and to exit press 'q' or 'Q' : ")
-    continue = gets.chomp.rstrip
-  end
-end 
-
 def check_palindrome(sen)
   sen.reverse.eql? sen
 end
 
-palindrome
+continue = 'y'
+until continue == 'q' || continue == 'Q' do
+  print ("enter the string to be checked : ")
+  statement = gets.chomp.rstrip.downcase
+  puts check_palindrome(statement) ? "String entered is Palindrome" : "String entered is not Palindrome" 
+  print("To continue press enter and to exit press 'q' or 'Q' : ")
+  continue = gets.chomp.rstrip
+end
+
