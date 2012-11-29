ae13cce37a59d015aa809c713ec46023a84b01d8
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-11-30 02:47:27 +0530

    modified

--- prime_using_step.rb
+++ prime_using_step.rb
@@ -1,23 +1,24 @@
-def find_prime
-  print ("Enter the End Limit: ")
-  end_limit = gets.to_i
-  1.step(end_limit) do |h|
-    prime = gen_prime(h)
+def generate_prime_list(end_limit)
+  1.step(end_limit) do |number|
+    prime = check_prime(number)
     if prime == 0
-      puts(h)
+      puts(number)
     end
-    h+=1
+    number+=1
   end  
 end
 
-def gen_prime(num)
-  2.step(num-1) do |h|
-    if num.remainder(h)==0
+def check_prime(number)
+  2.step(number-1) do |num|
+    if number.remainder(num)==0
       return 1
     end
   end
   return 0
 end
 	 
-find_prime
+print ("Enter the End Limit: ")
+end_limit = gets.to_i
+generate_prime_list(end_limit)
+
 
