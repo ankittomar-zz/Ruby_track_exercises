2ae9c0014c32d3dee479ddf4a8e5aa455601bb84
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-11-30 09:40:42 +0530

    modified regularex.rb

--- regularex.rb
+++ regularex.rb
@@ -1,12 +1,10 @@
-def get_text()
-  print("enter the text : ")
-  statement = gets.chomp
-  puts ("String before substitution : #{statement}")
-  puts ("string after substitution : #{replace_vowel(statement)}")
-end
-
 def replace_vowel(statement)
   statement.gsub(/[aeiou]/i, '*')
 end
-get_text
+
+print("enter the text : ")
+statement = gets.chomp
+puts ("String before substitution : #{statement}")
+puts ("string after substitution : #{replace_vowel(statement)}")
+
 
