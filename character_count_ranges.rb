9b0b2818aa9976d12cf7cce162088d1c4c9127bc
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-11-30 12:46:21 +0530

    character count

--- character_count_ranges.rb
+++ character_count_ranges.rb
@@ -10,10 +10,10 @@ def count_characters(text)
   special_char = 0
   digits = 0
   text.each_char do |c| 
-   if(('a'..'z')=== c) then lower_case += 1
-   elsif(('A'..'Z')=== c) then  upper_case+=1
-   elsif(('0'..'9')=== c ) then digits+=1
-   elsif(((58..64)=== c.ord || (33..47)=== c.ord) || (91..96)=== c.ord || (123..126) === c.ord) then special_char+=1 
+   if(('a'..'z') === c) then lower_case += 1
+   elsif(('A'..'Z') === c) then  upper_case += 1
+   elsif(('0'..'9') === c ) then digits += 1
+   elsif(((58..64) === c.ord || (33..47) === c.ord) || (91..96) === c.ord || (123..126) === c.ord) then special_char += 1 
    end 
   end
   puts ("Text used => #{text}")
