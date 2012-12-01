779144be831394db6d905b719047d0ce7661296a
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-12-01 17:34:11 +0530

    modified prime step

--- prime_using_step.rb
+++ prime_using_step.rb
@@ -1,18 +1,18 @@
 def generate_prime_list(end_limit)
 
-  print ("1  2")
+  print ("2")
   (3..end_limit).step(2) do |number|
     prime = check_prime(number)
     if prime == 0
       print("  #{number}")
     end
-    number+=1
+    number += 1
   end  
 end
 
 def check_prime(number)
   (3..number-1).step(2) do |num|
-    if number.remainder(num)==0
+    if number.remainder(num) == 0
       return 1
     end
   end
