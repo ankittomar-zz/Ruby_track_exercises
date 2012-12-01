830c1ac59813548fbf698012ea80bc77fb84ef69
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-12-01 15:23:43 +0530

    indentation for array_hash

--- array_hash.rb
+++ array_hash.rb
@@ -1,7 +1,7 @@
 def array_hash
-items_array = Array.new
-items_array = ['array','qwerty','qwer',123456,12345,'qwert',1234,123,'qwe',12,'qw',1234567,'qwertyu']
-array_to_hash(items_array)
+  items_array = Array.new
+  items_array = ['array','qwerty','qwer',123456,12345,'qwert',1234,123,'qwe',12,'qw',1234567,'qwertyu']
+  array_to_hash(items_array)
 end
 
 def array_to_hash(items)
