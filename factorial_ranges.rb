fa0e0332781246353c5733175cdb15de98ffdc3b
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2012-12-01 16:33:09 +0530

    factorial

--- factorial_ranges.rb
+++ factorial_ranges.rb
@@ -1,16 +1,17 @@
-def factorial
+def factorial_generator
   puts("enter the number whose factorial is to be calculated: ")
   number = gets.to_i
-  if number<0
+  factorial = generate_factorial(number) 
+  if generate_factorial(number) == false
     puts("Factorial of negative number cant be calculated please try again")
-    factorial
-  else
-    factorial = generate_factorial(number)
+    factorial_generator
+  else    
     puts ("Factorial of #{number} is = #{factorial}") 
   end
 end
 
 def generate_factorial(number)
+  return false if number < 0
   return 1 if number < 2  
   rang = 2..number
   factorial = 1 
@@ -18,6 +19,6 @@ def generate_factorial(number)
   factorial  
 end
 
-factorial
+factorial_generator
 
 
