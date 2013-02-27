2258c17c81230ea0f6bc18c5117e098d6f9f544a
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-02-27 21:41:21 +0530

    modified exercise 2;

--- java_script_exercises/Exercise 2/exercise2.js
+++ java_script_exercises/Exercise 2/exercise2.js
@@ -1,28 +1,31 @@
 function check_day(element) {
-    var checkbox_none = document.getElementsByClassName('cb2');
-    checkbox_none[0].checked = false;
-
+    var checkbox_none_arr = window.document.getElementsByClassName('cb2');
+    checkbox_none = checkbox_none_arr[0];
     checkbox_none.checked = false;  
+
     var check_boxes = document.getElementsByClassName('cb1');
     var number_of_checked_boxes = 0;
+    var check_boxes_length = check_boxes.length;
     message = "Only 3 days can be selected. You have already selected";
 
-    for each(var check_box in check_boxes)
-      {
+    for (i=0; i<check_boxes_length; i++ )
+      {   
+          check_box = check_boxes[i];    
           if (check_box.checked ) 
           {
               number_of_checked_boxes += 1;
-          }  
+          } 
       } 
 
-    if (number_of_checked_boxes >= 4)
-    {    
+    if (number_of_checked_boxes > 3)
+    {   
         element.checked = false;
-        for each(var checks_box in check_boxes)
+        for (i=0; i<check_boxes_length; i++)
         {
-            if(checks_box.checked == true)
+            check_box = check_boxes[i];
+            if(check_box.checked)
             {    
-                message = message.concat(',',checks_box.value);
+                message = message.concat(',',check_boxes[i].value);
             }
         }
         alert(message);
@@ -34,8 +37,10 @@ function check_day(element) {
 
 function check_none() {
     check_boxes = document.getElementsByClassName('cb1');
-    for each(var check_box in check_boxes)
+    check_boxes_length = check_boxes.length;
+    for (i=0; i<check_boxes_length; i++)
     {
+        check_box = check_boxes[i];
         check_box.checked = false;
     }
 };
