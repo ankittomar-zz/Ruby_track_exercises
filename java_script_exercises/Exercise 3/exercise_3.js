619744dbd74f6862cae52618cc5bfbfbb3d21940
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-03-10 16:15:40 +0530

    exercise 3 done

--- java_script_exercises/Exercise 3/exercise_3.js
+++ java_script_exercises/Exercise 3/exercise_3.js
@@ -19,43 +19,41 @@ function add_col_to_row(new_row) {
     new_email_col = new_row.insertCell(1);
     new_action_col = new_row.insertCell(2);
     
-    
-    //new_name_col.innerHTML = insert_text_field();
-    name_text_field = insert_text_field();
+    name_text_field = create_text_field();
     name_text_field.name = "name_txt";
     new_name_col.appendChild(name_text_field);
 
-    email_text_field = insert_text_field();
+    email_text_field = create_text_field();
     email_text_field.name = "email_field";
     new_email_col.appendChild(email_text_field); 
     
-    submit_button = insert_button();
-    submit_button.name = "submit_button";
-    submit_button.value = "Submit";
-    submit_button.onclick = function() {submit_line_item(this)};
-    submit_button.id = "submit_button_1"
+    submit_button = create_button();
     new_action_col.appendChild(submit_button);
-    
-
-    table_row_count = table_node.rows.length;
     
 }
 
-function insert_text_field() {
+function create_text_field() {
+    
     new_text_field = document.createElement("input");
     new_text_field.type = "text";
+    
     return new_text_field;
 }
 
-function insert_button() {
+function create_button() {
 
     new_button = document.createElement("input");
     new_button.type = "submit";
+    new_button.name = "submit_button";
+    new_button.value = "Save";
+    new_button.onclick = function() {submit_line_item(this)};
+    new_button.id = "submit_button_1"
+    
     return new_button;
 
 }
 
-function insert_link(link_text_value) {
+function create_link(link_text_value) {
 
     
     new_link = document.createElement('a');
@@ -77,17 +75,19 @@ function submit_line_item(element) {
 
 function replace_button_with_link(element) {
 
-    edit_link = insert_link('edit');
+    edit_link = create_link('edit');
     edit_link.onclick = function() {edit_data(this)};
-    delete_link = insert_link('Delete');
-    delete_link.onclick = function() {delete_data(this)};
-    ref = element.parentNode;
     
-    new_edit_link = element.parentNode.replaceChild(edit_link,element);
+    delete_link = create_link('Delete');
+    delete_link.onclick = function() {delete_row(this)};
+    
+    parent_element = element.parentNode;
+    
+    new_edit_link = parent_element.replaceChild(edit_link,element);
     new_node = document.createTextNode("  /  ");
     
-    ref.appendChild(new_node);
-    ref.appendChild(delete_link);
+    parent_element.appendChild(new_node);
+    parent_element.appendChild(delete_link);
 
 }
 
@@ -104,34 +104,26 @@ function submit_data(element) {
 }
 
 function edit_data(element) {
-    alert(element);
-
+    
     parent_node = element.parentNode.parentNode;
-    alert(parent_node);
-    name_data = parent_node.childNodes[0].innerHTML;
-    email_data = parent_node.childNodes[1].innerHTML;
-    name_text_field = insert_text_field();
-    name_text_field.name = "name_txt";
-    name_text_field.value = name_data;
+    
     name_col = parent_node.childNodes[0];
     email_col = parent_node.childNodes[1];
-    alert(name_text_field);
-    alert(name_col);
-    alert(email_col);
-    alert(name_col.childNodes[0]);
-
+    
+    name_data = name_col.innerHTML;
+    email_data = email_col.innerHTML;
+    
+    name_text_field = create_text_field();
+    name_text_field.name = "name_txt";
+    name_text_field.value = name_data;
     name_col.replaceChild(name_text_field,name_col.childNodes[0]);
 
-    email_text_field = insert_text_field();
+    email_text_field = create_text_field();
     email_text_field.name = "email_field";
     email_text_field.value = email_data;
     email_col.replaceChild(email_text_field,email_col.childNodes[0]);
 
-    submit_button = insert_button();
-    submit_button.name = "submit_button";
-    submit_button.value = "Submit";
-    submit_button.onclick = function() {submit_line_item(this)};
-    submit_button.id = "submit_button_1"
+    submit_button = create_button();
     action_col = element.parentNode;
     action_col.innerHTML = '';
     action_col.appendChild(submit_button);
@@ -140,10 +132,10 @@ function edit_data(element) {
     
 }
 
-function delete_data(element) {
+function delete_row(element) {
 
-    table =element.parentNode.parentNode.parentNode;
-    row = element.parentNode.parentNode.rowIndex;
-    table.deleteRow(row);
+    current_row  = element.parentNode.parentNode; 
+    table = current_row.parentNode;
+    table.deleteRow(current_row.rowIndex);
 }
 
