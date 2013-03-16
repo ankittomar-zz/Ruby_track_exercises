5135567ac918535b34bae8521fb0dd630407547a
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-03-17 01:42:31 +0530

    added exercise 9

---
+++ java_script_exercises/Exercise 9/exercise9.js
@@ -0,0 +1,21 @@
+function validate_number(){
+
+    var input_text = document.getElementById("input_field").value;
+    var result_field = document.getElementById("result");
+    alert(input_text);
+    
+    var number_regex = /^((\.)?([0-9]+)(\.[0-9]+)*)/;
+    alert(isNaN(input_text));
+    if(isNaN(input_text))
+    {
+        result_field.value = "false";
+        return false;
+
+    }
+    else
+    {
+        result_field.value = "true";
+        return true;
+    }
+
+}
