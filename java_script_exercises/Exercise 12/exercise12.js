ab988271d9f48410e3caf95d42d3ff6a1c477feb
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-03-22 15:13:11 +0530

    exercise 4 added

---
+++ java_script_exercises/Exercise 12/exercise12.js
@@ -0,0 +1,37 @@
+function show(element) {
+
+    child_node = element.parentNode.getElementsByTagName('ul');
+    
+    if (element.checked)
+    {  
+        state = true;
+        dis = "block";
+    }
+    else
+    { 
+        state = false;
+        dis = "none";
+    }
+
+    child_node[0].style.display = dis;
+    change_child_checkboxes_state(element,state);
+    scroll_to_show_child(element);
+}
+
+function change_child_checkboxes_state(element,state) {
+
+    child_elements = element.parentNode.getElementsByTagName("input");
+    number_of_child = child_elements.length;
+    for(i=0;i<number_of_child;i++)
+    {
+        child_elements[i].checked = state;
+    } 
+
+
+}
+
+
+function scroll_to_show_child(element) {
+
+    element.scrollIntoView(true);
+}
