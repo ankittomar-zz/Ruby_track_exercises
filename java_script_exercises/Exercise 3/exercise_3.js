5c6ab10c7cf01e7b33ae2abbc8d6a7b4650b34de
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-06-17 20:00:06 +0530

    modified exercise 5

--- java_script_exercises/Exercise 3/exercise_3.js
+++ java_script_exercises/Exercise 3/exercise_3.js
@@ -44,7 +44,7 @@ function create_submit_button() {
     new_submit_button.type = "submit";
     new_submit_button.name = "submit_button";
     new_submit_button.value = "Save";
-    new_submit_button.onclick = function() {submit_line_item(this)};
+    new_submit_button.onclick = function() { submit_line_item(this) };
     return new_submit_button;
 
 }
@@ -52,7 +52,7 @@ function create_submit_button() {
 function create_link(link_text_value) {
    
     var new_link = document.createElement('a');
-    new_link.setAttribute('href',"javascript:void(0)");
+    new_link.setAttribute('href', "javascript:void(0)");
     new_link.innerHTML = link_text_value;
     return new_link;
 
@@ -68,7 +68,7 @@ function submit_line_item(element) {
     if (name_column_element.childNodes[0].value != '' && email_column_element.childNodes[0].value != '')
         {
 
-            submit_data(name_column_element,email_column_element);
+            submit_data(name_column_element, email_column_element);
             replace_button_with_link(element);
         }
     else alert("Name and Email can't be blank");
@@ -79,13 +79,13 @@ function submit_line_item(element) {
 function replace_button_with_link(element) {
 
     var edit_link = create_link('edit');
-    edit_link.onclick = function() {edit_data(this)};
+    edit_link.onclick = function() { edit_data(this) };
     delete_link = create_link('Delete');
-    delete_link.onclick = function() {delete_row(this)};
+    delete_link.onclick = function() { delete_row(this) };
     
     var parent_element = element.parentNode;
     
-    var new_edit_link = parent_element.replaceChild(edit_link,element);
+    var new_edit_link = parent_element.replaceChild(edit_link, element);
     var new_node = document.createTextNode("  /  ");
     
     parent_element.appendChild(new_node);
@@ -93,7 +93,7 @@ function replace_button_with_link(element) {
 
 }
 
-function submit_data(name_column_element,email_column_element) {
+function submit_data(name_column_element, email_column_element) {
     
     
     var name_data = name_column_element.childNodes[0].value;
@@ -113,11 +113,11 @@ function edit_data(element) {
 
     var name_text_field = create_text_field('name_txt');
     name_text_field.value = name_data;
-    name_column.replaceChild(name_text_field,name_column.childNodes[0]);
+    name_column.replaceChild(name_text_field, name_column.childNodes[0]);
 
     var email_text_field = create_text_field('email_field');
     email_text_field.value = email_data;
-    email_column.replaceChild(email_text_field,email_column.childNodes[0]);
+    email_column.replaceChild(email_text_field, email_column.childNodes[0]);
 
     var submit_button = create_submit_button();
     var action_column = element.parentNode;
