aa41706b979d2dd5a7f8972e728d6fa6477e99f7
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-06-17 19:48:41 +0530

    fixed bugs related to exercise 5

--- java_script_exercises/Exercise 5/exercise5.js
+++ java_script_exercises/Exercise 5/exercise5.js
@@ -4,15 +4,15 @@ function get_name() {
     last_name = prompt("please enter your Last name");
     
     if( first_name == null || first_name.trim() == '' || last_name == null || last_name.trim() == '' )
-        {
-            alert("First Name and Last Name can't be Null or space characters. Please try Again ");
-            document.location.reload();  
-        }
+    {
+        alert("First OR and Last Name can't be Null or space characters. Please try Again ");
+        document.location.reload();  
+    }
     else  
-        {    
-            message = "hello " + first_name + " " + last_name;
-            alert(message);
-            document.getElementById("para_1").innerHTML=message;
-        }
+    {    
+        message = "hello " + first_name + " " + last_name;
+        alert(message);
+        document.getElementById("welcome_message_paragraph").innerHTML=message;
+    }
 }
 
