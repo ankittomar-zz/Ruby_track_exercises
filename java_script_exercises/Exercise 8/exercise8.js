739accac25aa6b446c26e35448f8778f5b7ab0f1
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-06-18 18:13:44 +0530

    changed regex to check email format in exercise 7

--- java_script_exercises/Exercise 8/exercise8.js
+++ java_script_exercises/Exercise 8/exercise8.js
@@ -3,7 +3,7 @@ function check_format() {
     var email_text = document.getElementById("email_id").value;
     var home_page_url = document.getElementById("home_url").value;
     
-    var email_regex = /^(?:\w+\.?)*\w+@(?:\w+\.?)*\w+$/
+    var email_regex = /^(\w+)(\.?\w+)*(\w+)(@)(\w+)(\.+)(\w+)$/
     var home_url_regex = /^(https?:\/\/)?(www.)([\da-z\.-]+)\.([a-z]{2,6})(\.[a-z])?$/  
     
     if (email_regex.test(email_text)) validation_email = true;
