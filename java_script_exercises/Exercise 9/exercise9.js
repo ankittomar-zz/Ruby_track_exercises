f7f8ec19b59ef5c1f4c909fd77b9f559bd6974ca
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-06-19 15:01:39 +0530

    Removed alert messages

--- java_script_exercises/Exercise 9/exercise9.js
+++ java_script_exercises/Exercise 9/exercise9.js
@@ -2,10 +2,6 @@ function validate_number(){
 
     var input_text = document.getElementById("input_field").value;
     var result_field = document.getElementById("result");
-    alert(input_text);
-    
-   // var number_regex = /^((\.)?([0-9]+)(\.[0-9]+)*)/;
-    alert(isNaN(input_text));
     if(isNaN(input_text))
     {
         result_field.value = "false";
