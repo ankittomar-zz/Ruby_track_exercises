5389181c6c69b2920e7bdbde678c26ae52074abc
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-06-19 16:29:31 +0530

    exercise 10 regex updated

--- java_script_exercises/Exercise 10/exercise10.js
+++ java_script_exercises/Exercise 10/exercise10.js
@@ -3,30 +3,30 @@ function extract_domain_sub_domain() {
     var domain, url, subdomain, url_text, url_format, url_format_regex,input; 
     
     input = document.getElementById("url").value;
-    url_format_regex = /(^((http)s?:\/\/)?www\.[a-z]+\.(([a-z]+)|([a-z]+\.[a-z]+)|([a-z]+\.[a-z]+\.[a-z]+))(\/\w+)*$)/i
-    url_format = /(www\.([a-z]+)(\.[a-z]+)(\.[a-z]+)?(\.[a-z]+)?)/
+    url_format_regex = /(^((http)s?:\/\/)?(www\.)?[a-z]+\.(([a-z]+)|([a-z]+\.[a-z]+)|([a-z]+\.[a-z]+\.[a-z]+))(\/.*)*$)/i
+    url_format = /((www\.)?([a-z]+)(\.[a-z]+)(\.[a-z]+)?(\.[a-z]+)?)/
     url_text = input.match(url_format_regex);
     
     if(url_text != null)
     {       
         url = url_text[0].match(url_format);
-        if(RegExp.$4 == "" && RegExp.$5 == "")
+        if(RegExp.$5 == "" && RegExp.$6 == "")
         {
 
-            domain = RegExp.$2+RegExp.$3;
+            domain = RegExp.$3+RegExp.$4;
             
         }
-        else if ( RegExp.$4 != "" && RegExp.$5 == "")
+        else if ( RegExp.$5 != "" && RegExp.$6 == "")
         {
-            subdomain = RegExp.$2;
-            domain = RegExp.$3 +RegExp.$4;
+            subdomain = RegExp.$3;
+            domain = RegExp.$4 +RegExp.$5;
             
         }
 
-        else if(RegExp.$5 != "")
+        else if(RegExp.$6 != "")
         {
-            subdomain = RegExp.$2;
-            domain = RegExp.$3+RegExp.$4+RegExp.$5;
+            subdomain = RegExp.$3;
+            domain = RegExp.$4+RegExp.$5+RegExp.$6;
         }
 
          
