47f697dd143aba998b77b22f5aca71c558522dbb
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-07-02 18:45:26 +0530

    modified exercise 9 added window.onload and binded onclick event through addEventListener

--- java_script_exercises/Exercise 9/exercise9.js
+++ java_script_exercises/Exercise 9/exercise9.js
@@ -2,15 +2,17 @@ var validate_number = function (event){
 
     var input_text = document.getElementById("input_field").value;
     var result_field = document.getElementById("result");
-    if(isNaN(input_text))
+    var num_regex = /^\d+$/;
+
+    if(input_text.match(num_regex) == null)
     {
         result_field.value = "false";
         event.preventDefault();
     }
     else
     {
-        return result_field.value = "true";
-        return true;
+        result_field.value = "true";
+        
     }
 
 }
