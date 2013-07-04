7bdd1a795c430b576299170b60e18720995b1da8
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-07-04 17:43:55 +0530

    modified exercise 9 to implement via prototype

--- java_script_exercises/Exercise 9/exercise9.js
+++ java_script_exercises/Exercise 9/exercise9.js
@@ -1,23 +1,38 @@
-var validate_number = function (event){
+var Validate = function() {
+    this.bindSubmit();
+}
 
-    var input_text = document.getElementById("input_field").value;
-    var result_field = document.getElementById("result");
-    var num_regex = /^\d+$/;
+Validate.prototype = {
+    validate_number: function (event){
+        var input_text = document.getElementById("input_field").value;
+        var result_field = document.getElementById("result");
+        var num_regex = /^\d+$/;
 
-    if(input_text.match(num_regex) == null)
-    {
-        result_field.value = "false";
-        event.preventDefault();
+        if(input_text.match(num_regex) == null)
+        {
+            result_field.value = "false"
+            event.preventDefault();          
+            //return false;
+        }
+        else
+        {
+            result_field.value = "true";
+            
+        }
+
+    },
+
+    bindSubmit: function () {
+        var that = this;
+        var submit_button = document.getElementById("submit_form");
+        submit_button.addEventListener("click", that.validate_number,false)  ;
     }
-    else
-    {
-        result_field.value = "true";
-        
-    }
 
+     
 }
 
 window.onload = function () {
-    var submit_button = document.getElementById("submit_form");
-    submit_button.addEventListener("click",validate_number,false);
-}   
+       
+        validate = new Validate ();
+} 
+
