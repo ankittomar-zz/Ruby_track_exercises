c19dbc275e38155d265de793bb55d169d8e8f403
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-07-04 18:09:16 +0530

    modified exercise 9 to implement via prototype

--- java_script_exercises/Exercise 9/exercise9.js
+++ java_script_exercises/Exercise 9/exercise9.js
@@ -6,12 +6,13 @@ Validate.prototype = {
     validate_number: function (event){
         var input_text = document.getElementById("input_field").value;
         var result_field = document.getElementById("result");
-        var num_regex = /^\d+$/;
+        var num_regex = /^\d+\.{0,1}\d+$/;
 
         if(input_text.match(num_regex) == null)
         {
             result_field.value = "false"
-            event.preventDefault();          
+            event.preventDefault(); 
+
             //return false;
         }
         else
@@ -25,7 +26,7 @@ Validate.prototype = {
     bindSubmit: function () {
         var that = this;
         var submit_button = document.getElementById("submit_form");
-        submit_button.addEventListener("click", that.validate_number,false)  ;
+        submit_button.addEventListener("submit", that.validate_number)  ;
     }
 
      
