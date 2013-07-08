a967efaa1f5dc2e0881632f1d352591d0a0f8fc2
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-07-08 20:20:49 +0530

    fixed issues and implemented using prototype

--- java_script_exercises/Exercise 12/exercise12.js
+++ java_script_exercises/Exercise 12/exercise12.js
@@ -6,7 +6,6 @@ var TreeCheckboxList = function(){
 TreeCheckboxList.prototype = {
 
     showChild: function() {
-
         var element = this;
         var child_node = element.parentNode.getElementsByTagName('ul');
 
@@ -22,32 +21,30 @@ TreeCheckboxList.prototype = {
         }
 
         child_node[0].style.display = display;
-        tree.change_child_checkboxes_state(element, state);
-        tree.scroll_to_show_child(element);
-        },
+        tree.changeChildCheckboxesState(element, state);
+        tree.scrollToShowChild(element);
+    },
 
-    change_child_checkboxes_state: function(element, state) {
+    changeChildCheckboxesState: function(element, state) {
 
-        child_elements = element.parentNode.getElementsByTagName("input");
-        number_of_child = child_elements.length;
-        for(var i = 0; i < number_of_child; i++)
+        var child_elements = element.parentNode.getElementsByTagName("input");
+        for(var i = 0; i < child_elements.length; i++)
         {
             child_elements[i].checked = state;
         } 
     },
 
 
-    scroll_to_show_child: function(element) {
+    scrollToShowChild: function(element) {
 
         element.scrollIntoView(true);
     },
 
     bindClick: function(){
-        var that = this;
         var parent_checkbox = document.getElementsByClassName("parent_box");
         for(var i = 0; i < parent_checkbox.length; i++)
         {   
-            parent_checkbox[i].addEventListener("click", that.showChild);
+            parent_checkbox[i].addEventListener("click", this.showChild);
 
         }
     }
