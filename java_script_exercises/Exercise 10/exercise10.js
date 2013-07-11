2a5206ce3781c87569cc74805faf98e6fc81fc36
AnkitTomar <ankittomar@ankittomar-HP-ProBook-4530s.(none)>
2013-07-11 14:15:44 +0530

    fixed issues related to exercise 10

--- java_script_exercises/Exercise 10/exercise10.js
+++ java_script_exercises/Exercise 10/exercise10.js
@@ -7,45 +7,45 @@ Domain.prototype = {
         var domain, url, subdomain, url_text, url_format, url_format_regex, input; 
         
         input = document.getElementById("url").value.trim();
-        url_protocol_format_regex = /(^((http)s?:\/\/)?(www\.)?[a-z]+\.(([a-z]+)|([a-z]+\.[a-z]+)|([a-z]+\.[a-z]+\.[a-z]+))(\/.*)*$)/i
-        url_regex = /((www\.)?([a-z]+)(\.)([a-z]+)(\.[a-z]+)?(\.[a-z]+)?)/
+        url_protocol_format_regex = /^((http)s?:\/\/)?(www\.)?[a-z]+\.(([a-z]+)|([a-z]+\.[a-z]+)|([a-z]+\.[a-z]+\.[a-z]+))(\/.*)*$/i
+        url_regex = /(www\.)?([a-z]+)(\.)([a-z]+)(\.[a-z]+)?(\.[a-z]+)?/
         url_text = input.match(url_protocol_format_regex);
         
-        if(url_text != null)
-        {       
-            url = url_text[0].match(url_regex);
-            if(RegExp.$6 == "" && RegExp.$7 == "")
-            {
-
-                domain = RegExp.$3 + RegExp.$4 + RegExp.$5;
-                
-            }
-            else if ( RegExp.$6 != "" && RegExp.$7 == "")
-            {
-                subdomain = RegExp.$3;
-                domain = RegExp.$5 + RegExp.$6;
-                
-            }
-
-            else if(RegExp.$7 != "")
-            {
-                subdomain = RegExp.$3;
-                domain = RegExp.$5 + RegExp.$6 + RegExp.$7;
-            }
-
-             
-            if(subdomain!=null)
-            {
-              alert("Domain : " + domain);
-              alert("Sub domain : "+ subdomain);
-            }
-            else alert( "Domain :  " + domain);
+        if(!url_text)
+        {   
+            alert("Invalid Url Please try again");
+            return false;
         }   
           
         else
         {
-            alert("Invalid Url Pleasae try again");
-            return false;
+            url = url_text[0].match(url_regex);
+
+            if(!url[5] && !url[6])
+            {
+
+                domain = url[2] + url[3] + url[4];
+                
+            }
+            else if ( !!url[5] && !url[6])
+            {
+                subdomain = url[2];
+                domain = url[4] + url[5];
+                
+            }
+
+            else if(!!url[6])
+            {
+                subdomain = url[2];
+                domain = url[4] + url[5] + url[6];
+            }
+
+             
+            if(!!subdomain)
+            {
+              alert("Domain : " + domain + ", " + "Sub domain : "+ subdomain);
+            }
+            else alert( "Domain :  " + domain);
         }
     },
 
